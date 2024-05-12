21ed97749b219c61413009a1fdfceb15daa844d5
Mason <Maestro720@vk.com>
2024-05-12 19:56:19 +0300

    skip one test, one stubs

--- cypress/e2e/article/article-details.cy.ts
+++ cypress/e2e/article/article-details.cy.ts
@@ -13,19 +13,26 @@ describe('Пользователь заходит на страницу стат
       cy.removeArticle(currentArticleId);
   })
   //Создали статью - протестировали все что нужно - удалили статью
-      it('И видит содержимое статьи', () => {
+      it.skip('И видит содержимое статьи', () => {
           cy.getByTestId('ArticleDetails.Info').should('exist');
       })
-      it('И видит рекоммендации', () => {
+      it.skip('И видит рекоммендации', () => {
           cy.getByTestId('ArticleRecomendationsList').should('exist');
       })
-      it('И оставляет комментарии', () => {
+      it.skip('И оставляет комментарии', () => {
           cy.getByTestId('ArticleDetails.Info');
           cy.getByTestId('AddCommentForm').scrollIntoView();
           cy.addComment('text');
           cy.getByTestId('CommentCard.Content').should('have.length', 1);
       })
-      it('И ставит оценку', () => {
+      it.skip('И ставит оценку', () => {
+        cy.getByTestId('ArticleDetails.Info');
+        cy.getByTestId('RatingCard').scrollIntoView();
+        cy.setRate(5, 'feedback');
+        cy.get('[data-selected=true]').should('have.length', 5);
+    })
+      it.skip('И ставит оценку (пример со стабом на фикстурах)', () => {
+        cy.intercept('GET', '**/articles/*', { fixture: 'article-details.json' })
         cy.getByTestId('ArticleDetails.Info');
         cy.getByTestId('RatingCard').scrollIntoView();
         cy.setRate(5, 'feedback');

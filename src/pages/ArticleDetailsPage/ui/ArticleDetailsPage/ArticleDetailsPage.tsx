f66cb8861e63038e9dee36881f79b1959a22b507
Mason <Maestro720@vk.com>
2024-05-18 17:20:32 +0300

    add toggle flags and auto remove script

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -10,20 +10,32 @@ import { VStack } from "@/shared/ui/Stack";
 import { ArticleRecomendationsList } from "@/features/articleRecomendationsList";
 import { ArticleDetailsComments } from "../ArticleDetailsComments/ArticleDetailsComments";
 import { ArticleRating } from "@/features/articleRating";
-import { getFeatureFlag } from "@/shared/features";
+import { getFeatureFlag, toggleFeatures } from "@/shared/features";
+import { Card } from "@/shared/ui/Card/Card";
 
 const reducers: ReducersList = {
     articleDetailsPage: articleDetailsPageReducer
 }
 
+const Counter = () => <div>Old Counter</div>
+const CounterRedesigned = () => <div>New Counter</div>
+
+
 const ArticleDetailsPage = () => {
     const { id } = useParams<{id: string}>();
     const {t} = useTranslation('article');
     const isArticleRatingEnabled = getFeatureFlag('isArticleRatingEnabled');
+    const isCounterEnabled = getFeatureFlag('isCounterEnabled');
 
     if(!id){
         return null;
     }
+
+    const articleRatingCard = toggleFeatures({
+        name: 'isCounterEnabled',
+        on: () => <ArticleRating articleId={id} />,
+        off: () => <Card>Оценка статей скоро появится</Card>
+    })
 
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
@@ -31,7 +43,7 @@ const ArticleDetailsPage = () => {
                 <VStack gap='16' max>
                     <ArticleDetailsPageHeader/>
                     <ArticleDetails id={id}/>
-                    {isArticleRatingEnabled && <ArticleRating articleId={id} />}
+                    {articleRatingCard}
                     <ArticleRecomendationsList/>              
                     <ArticleDetailsComments id={id}/>
                 </VStack>

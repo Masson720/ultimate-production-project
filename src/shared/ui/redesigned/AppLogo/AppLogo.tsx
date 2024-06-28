eeac8db451773ca4b496ae29bf456e154ecef79b
Mason <Maestro720@vk.com>
2024-06-28 17:23:50 +0300

    mini fixes

--- src/shared/ui/redesigned/AppLogo/AppLogo.tsx
+++ src/shared/ui/redesigned/AppLogo/AppLogo.tsx
@@ -14,14 +14,14 @@ export const AppLogo = memo((props: AppLogoProps) => {
     const { className, size = 50 } = props;
     return (
         <HStack max justify="center" className={classNames(cls.AppLogo, {}, [className])}>
-            <div className={cls.gradientBig}></div>
-            <div className={cls.gradientSmall}></div>
             <AppSvg 
                 width={size} 
                 height={size} 
                 color='black'
                 className={cls.appLogo} 
-            />
+            />            
+            <div className={cls.gradientBig}></div>
+            <div className={cls.gradientSmall}></div>
         </HStack>
     )
 })
